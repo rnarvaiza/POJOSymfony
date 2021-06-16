ad090f23f7e65379028dfe478191f58443fdefdd
rnarvaiza <rnm7687@gmail.com>
2021-06-16 18:37:18 +0200

    forms and validations done

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller\Api;
 
+
 use App\Entity\Book;
+use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+
 
 class BooksController extends AbstractFOSRestController
 {
@@ -30,13 +32,21 @@ class BooksController extends AbstractFOSRestController
      */
 
     public function postAction(
-        EntityManagerInterface $em
+        EntityManagerInterface $em,
+        Request $request
     )
     {
         $book = new Book();
-        $book->setTitle('titulo de prueba POST');
-        $em->persist($book);
-        $em->flush();
-        return $book;
+        $form = $this->createForm(BookFormType::class, $book);
+        $form->handleRequest($request);
+
+        //Con isSubmitted, la función getName del BookFormType ya nos indica si pasa o no pasa el submit.
+        //Con isValid, si cumple la validación de config/validator/Book.yaml nos devuelve un $book, sino un $form que fos rest bundle es capaz de serializar con el contenido del error.
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($book);
+            $em->flush();
+            return $book;
+        }
+        return $form;
     }
 }
