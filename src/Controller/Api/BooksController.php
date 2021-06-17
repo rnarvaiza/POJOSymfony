57d86d3fc81535f28f9d4efbc7ab045034a2963a
rnarvaiza <rnm7687@gmail.com>
2021-06-17 19:11:55 +0000

    DTO y Carga de imagenes

--- src/Controller/Api/BooksController.php
+++ src/Controller/Api/BooksController.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
+use League\Flysystem\FilesystemOperator;
 
 
 class BooksController extends AbstractFOSRestController
@@ -27,6 +28,7 @@ class BooksController extends AbstractFOSRestController
         return $bookRepository->findAll();
     }
 
+
     /**
      * @Rest\Post(path="/books")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
@@ -34,7 +36,8 @@ class BooksController extends AbstractFOSRestController
 
     public function postAction(
         EntityManagerInterface $em,
-        Request $request
+        Request $request,
+        FilesystemOperator $defaultStorage
     )
     {
         $bookDto = new BookDto();
@@ -44,8 +47,13 @@ class BooksController extends AbstractFOSRestController
         //Con isSubmitted, la función getName del BookFormType ya nos indica si pasa o no pasa el submit.
         //Con isValid, si cumple la validación de config/validator/BookDto.yaml nos devuelve un $bookDto, sino un $form que fos rest bundle es capaz de serializar con el contenido del error.
         if($form->isSubmitted() && $form->isValid()) {
+            $extension = explode('/', mime_content_type($bookDto->base64Image))[1];
+            $data = explode(',', $bookDto->base64Image);
+            $filename = sprintf('%s.%s', uniqid('book_', true), $extension);
+            $defaultStorage->write($filename, base64_decode($data[1]));
             $book = new Book();
             $book->setTitle($bookDto->title);
+            $book->setImage($filename);
             $em->persist($book);
             $em->flush();
             return $book;
